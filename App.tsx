e4b674547da803d864fe2e80b45df4532c052292
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2026-07-06 17:11:41 +0300

    chore: reproduction

--- App.tsx
+++ App.tsx
@@ -1,18 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {useState} from 'react';
+import {StyleSheet, TextInput, View, Text, TextInputChangeEvent} from 'react-native';
 
-export default function App() {
+export default function App() { 
+  const [event, setEvent] = useState<TextInputChangeEvent['nativeEvent'] & {selection?: any} | null>(null);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <TextInput 
+        style={styles.input}
+        placeholder="Phone"
+        autoComplete="tel"
+        onChange={e => setEvent(e.nativeEvent)}
+      />
+
+      <Text style={styles.logs}>
+        [selection]: {JSON.stringify(event?.selection)}
+      </Text>
+      <Text style={styles.logs}>
+        [value]: {event?.text}
+      </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 64,
+    display: 'flex',
+    gap: 10,
+    
   },
+  logs: {
+    fontSize: 24,
+  },
+  input: {
+    height: 40,
+    padding: 5,
+    marginHorizontal: 8,
+    borderWidth: 1,
+  }
 });
